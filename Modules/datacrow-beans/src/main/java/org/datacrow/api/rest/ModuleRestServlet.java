de22a0b4a238d2a2fe5222ccaca20a2466813231
rjvanderwaals <95626792+rjvanderwaals@users.noreply.github.com>
2023-02-01 20:34:29 +0100

    REST API changes

--- Modules/datacrow-beans/src/main/java/org/datacrow/api/rest/ModuleRestServlet.java
+++ Modules/datacrow-beans/src/main/java/org/datacrow/api/rest/ModuleRestServlet.java
@@ -1,8 +1,18 @@
 package org.datacrow.api.rest;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import org.datacrow.api.rest.json.JsonField;
+import org.datacrow.api.rest.json.JsonModule;
+import org.datacrow.core.modules.DcModule;
+import org.datacrow.core.modules.DcModules;
+import org.datacrow.core.objects.DcField;
+import org.datacrow.core.server.serialization.SerializationHelper;
 import org.datacrow.core.utilities.CoreUtilities;
+
+import com.google.gson.Gson;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -12,22 +22,52 @@ import jakarta.servlet.http.HttpServletResponse;
 public class ModuleRestServlet extends HttpServlet {
 
     private String getModules() {
-        return "modules";
+        Collection<JsonModule> modules = new ArrayList<>();
+        
+        for (DcModule module : DcModules.getModules()) {
+            if (DcModules.isTopModule(module.getIndex()))
+                modules.add(new JsonModule(module));
+        }
+        
+        Gson gson = SerializationHelper.getInstance().getGson();
+        return gson.toJson(modules);
     }
 
-    private String getModule(String idx) {
-        return "module " + idx;
+    private String getModule(int idx) {
+        JsonModule module = new JsonModule(DcModules.get(idx));
+        Gson gson = SerializationHelper.getInstance().getGson();
+        return gson.toJson(module);
+    }
+    
+    private String getFields(int idx) {
+        DcModule module = DcModules.get(idx);
+        
+        Collection<JsonField> fields = new ArrayList<>();
+        for (DcField field : module.getFields()) {
+            fields.add(new JsonField(field));
+        }
+        
+        Gson gson = SerializationHelper.getInstance().getGson();
+        return gson.toJson(fields);
     }
     
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String id = (String) request.getParameter("id");
+        
+        String url = request.getRequestURI();
+        
         String json = "";
         
-        if (CoreUtilities.isEmpty(id)) {
-            json = getModules();
-        } else {
-            json = getModule(id);
+        if (url.endsWith("/modules")) {
+            String id = (String) request.getParameter("id");
+            
+            if (CoreUtilities.isEmpty(id))
+                json = getModules();
+            else
+                json = getModule(Integer.valueOf(id));
+        } else if (url.endsWith("/fields")) {
+            String moduleIndex = (String) request.getParameter("module");
+            json = getFields(Integer.valueOf(moduleIndex));
         }
         
         response.getOutputStream().println(json);
