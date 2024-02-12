860c8c0ddfc2c2861d9564d40263868f47f1c1cf
rjvanderwaals <robertjan@vanderwaalsbv.nl>
2024-02-12 10:03:49 +0100

    Change in version numbering

--- Modules/datacrow-server/src/main/java/org/datacrow/server/upgrade/SystemUpgrade.java
+++ Modules/datacrow-server/src/main/java/org/datacrow/server/upgrade/SystemUpgrade.java
@@ -102,7 +102,7 @@ public class SystemUpgrade {
                 checkAudioTables();
             }
             
-            if (dbInitialized && v.isOlder(new Version(4, 12, 0, 0))) {
+            if (dbInitialized && v.isOlder(new Version(5, 0, 0, 0))) {
             	if (DcConfig.getInstance().getOperatingMode() == DcConfig._OPERATING_MODE_SERVER) {
             		new ImageConverter(ImageConverter._UPGRADE_CONVERSION);
             	} else if (DcConfig.getInstance().getOperatingMode() == DcConfig._OPERATING_MODE_STANDALONE) {
