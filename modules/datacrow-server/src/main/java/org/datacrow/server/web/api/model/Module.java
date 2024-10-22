af8ada05c1e3838525c31e808e35ca5c2ab71114
rwaals <info@datacrow.org>
2024-10-22 14:05:27 +0200

    Better module list

--- modules/datacrow-server/src/main/java/org/datacrow/server/web/api/model/Module.java
+++ modules/datacrow-server/src/main/java/org/datacrow/server/web/api/model/Module.java
@@ -18,31 +18,43 @@ public class Module {
 	private final String name;
 	@JsonProperty("icon")
 	private String icon;
-	
+
 	@JsonProperty("children")
 	private Collection<Module> children = new ArrayList<Module>();
-	
+
 	public Module(int index, String name, DcImageIcon icon) {
 		this.index = index;
 		this.name = name;
 		this.icon = icon == null ? null : String.valueOf(Base64.encode(icon.getBytes()));
-		
-		for (DcModule child : DcModules.getReferencedModules(index))
-			children.add(new Module(child.getIndex(), child.getLabel(), child.getIcon32()));
+
+		for (DcModule child : DcModules.getReferencedModules(index)) {
+
+			if (child.isEnabled() && 
+		       !child.isSelectableInUI() && // if this is set, it is already added as a main module
+				child.getIndex() != index && 
+				child.getType() != DcModule._TYPE_PROPERTY_MODULE &&
+				child.getType() != DcModule._TYPE_EXTERNALREFERENCE_MODULE &&
+				child.getIndex() != DcModules._CONTACTPERSON &&
+			    child.getIndex() != DcModules._TAG &&
+				child.getIndex() != DcModules._CONTAINER) {
+
+				children.add(new Module(child.getIndex(), child.getLabel(), child.getIcon32()));
+			}
+		}
 	}
-	
+
 	public int getIndex() {
 		return index;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getIcon() {
 		return icon;
 	}
-	
+
 	public Module[] getChildren() {
 		Module[] moduleArray = new Module[children.size()];
 		moduleArray = children.toArray(moduleArray);
