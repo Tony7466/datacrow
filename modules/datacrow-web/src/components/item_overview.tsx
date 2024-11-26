b7882645586410f0de923491ca119426bcd2613e
rwaals <info@datacrow.org>
2024-11-26 14:34:43 +0100

    Layout changes and added filter button

--- modules/datacrow-web/src/components/item_overview.tsx
+++ modules/datacrow-web/src/components/item_overview.tsx
@@ -8,13 +8,18 @@ import Pagination from './pagination';
 export function ItemOverview() {
 
 	const currentModule = useCurrentModule();
-
 	const [items, setItems] = useState<Item[]>([]);
+	const [searchTerm, setSearchTerm] = useState<String>();
 
-	useEffect(() => {
-		currentModule &&
-		fetchItems(currentModule!).then((data) => setItems(data));
-	}, [currentModule]);
+	if (searchTerm) {
+		useEffect(() => {
+			currentModule && fetchItems(currentModule!).then((data) => setItems(data));
+		}, [currentModule]);
+	} else {
+		useEffect(() => {
+			currentModule && fetchItems(currentModule!).then((data) => setItems(data));
+		}, [currentModule]);
+	}
 
 	const [currentPage, setCurrentPage] = useState(1);
 	const [itemsPerPage, setItemsPerPage] = useState(30);
@@ -34,38 +39,48 @@ export function ItemOverview() {
 		setCurrentPage(1);
 	};
 
+	function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		let formData = new FormData(event.currentTarget);
+		let searchFor = formData.get("searchFor") as string;
+		setSearchTerm(searchFor);
+	}
+
 	return (
 		<div className="py-20 bg-slate-900 h-full">
-			<div>
-				<div style={{ right: "0px" }}>
-					{currentModule && <PagesDropdown
-						title={`Items per page: ${itemsPerPage}`}
-						options={itemsPerPageOptions}
-						handleSelectOption={(option: string) => setItemsPerPage(+option)}
-					/>}
+			
+			<form onSubmit={handleSubmit}>
+				<div className="input-group mb-3">
+					<input type="text" name="searchFor" />
+					<Button className="search-button" type="submit">search</Button>
 				</div>
-
-				<div style={{ display: "flex", flexWrap: "wrap" }}>
-					{currentItems!.map((item) => (
-						<Card style={{ width: '18rem' }} key={"card" + item.id}>
-							<Card.Body>
-								{item.imageUrl ? <Card.Img src={item.scaledImageUrl} /> : <div style={{ height: '300px' }} />}
-							</Card.Body>
-							<Card.Header style={{ height: '112px' }} >{item.name}</Card.Header>
-							<Card.Footer>
-								<Button variant="primary">open</Button>
-							</Card.Footer>
-						</Card>
-					))}
-				</div>
-
-				<Pagination
-					itemsPerPage={itemsPerPage}
-					totalItems={totalItems}
-					currentPage={currentPage}
-					paginate={paginate}
-				/>
+			</form>
+			
+			<div style={{ right: "0px" }}>
+				{currentModule && <PagesDropdown
+					title={`Items per page: ${itemsPerPage}`}
+					options={itemsPerPageOptions}
+					handleSelectOption={(option: string) => setItemsPerPage(+option)}
+				/>}
 			</div>
+
+			<div style={{ display: "flex", flexWrap: "wrap" }}>
+				{currentItems!.map((item) => (
+					<Card style={{ width: '18rem' }} key={"card" + item.id}>
+						<Card.Body>
+							{item.imageUrl ? <Card.Img src={item.scaledImageUrl} /> : <div style={{ height: '300px' }} />}
+						</Card.Body>
+						<Card.Header style={{ height: '112px' }} >{item.name}</Card.Header>
+					</Card>
+				))}
+			</div>
+
+			<Pagination
+				itemsPerPage={itemsPerPage}
+				totalItems={totalItems}
+				currentPage={currentPage}
+				paginate={paginate}
+			/>
 		</div>
 	);
 }
