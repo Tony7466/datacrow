00c52f8b73aaca61f6b08b4cabf61c340b1cad60
rwaals <info@datacrow.org>
2025-03-13 17:35:34 +0100

    web; versioning + combined change log

--- modules/datacrow-core/src/main/java/org/datacrow/core/Version.java
+++ modules/datacrow-core/src/main/java/org/datacrow/core/Version.java
@@ -34,7 +34,7 @@ import java.util.StringTokenizer;
  */
 public class Version implements Comparable<Version> {
 
-	private String edition = " BETA V";
+	private String edition = " Release Cadidate 1.0";
 	
     private int minor = 0;
     private int major = 0;
