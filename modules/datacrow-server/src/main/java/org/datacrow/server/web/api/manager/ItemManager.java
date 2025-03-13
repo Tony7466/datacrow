abce33a38b124282d8f627a9e2a2d7432895f453
rwaals <info@datacrow.org>
2025-03-13 11:08:18 +0100

    web; fix

--- modules/datacrow-server/src/main/java/org/datacrow/server/web/api/manager/ItemManager.java
+++ modules/datacrow-server/src/main/java/org/datacrow/server/web/api/manager/ItemManager.java
@@ -169,7 +169,7 @@ public class ItemManager {
 				}
 			} else if (field.getValueType() == DcRepository.ValueTypes._ICON) {
 				String base64 = (String) newValue;
-				if (!CoreUtilities.isEmpty(oldValue) && base64.indexOf("base64,") > 0) // new values contain the text base64 in the data string
+				if (!CoreUtilities.isEmpty(base64) && base64.indexOf("base64,") > 0) // new values contain the text base64 in the data string
 					newValue = base64.substring(base64.indexOf("base64,") + 7);
 				else if (!CoreUtilities.isEmpty(oldValue)) // no new value has been submitted! the new value is basically the old value 
 					newValue = oldValue;
